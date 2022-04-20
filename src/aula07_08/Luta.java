90b83f15ca6f75a03fe6cccf1187445dd8071676
GuilhermeN <guilhermesouzanastoo@hotmail.com>
2022-04-20 15:06:06 -0300

    aula 7b

---
+++ src/aula07_08/Luta.java
@@ -0,0 +1,18 @@
+package aula07_08;
+
+public class Luta {
+    // Atributos
+    private Lutador desafiado;
+    private Lutador desafiante;
+    private int rounds;
+    private boolean aprovada;
+
+    // Métodos
+    public void marcarLuta() {
+
+    }
+
+    public void lutar() {
+        
+    }
+}
